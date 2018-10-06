45d2852329af966f8bdcf3185ba66d9bc1456920
ddori <ddorigod@gmail.com>
2018-10-07 00:14:14 +0900

    모델 세부명 등록 & gitignore 파일 수정

--- analysis/models.py
+++ analysis/models.py
@@ -26,32 +26,35 @@ class SurveyModel(models.Model):
         ('u', '대학교 졸업'),
     )
 
-    name = models.CharField(max_length=128)
-    birth_date = models.DateField()
+    name = models.CharField(max_length=128, verbose_name='이름')
+    birth_date = models.DateField(verbose_name='생일')
     sex = models.CharField(
         max_length=1,
         choices=SEX_CHOICES,
         default='m',
+        verbose_name='성별',
     )
     address = models.CharField(
         max_length=2,
         choices=ADDRESS_CHOICES,
         default='1',
+        verbose_name='주소(행정구역)',
     )
-    address_detail = models.CharField(max_length=256)
-    phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(null=True, blank=True)
+    address_detail = models.CharField(max_length=256, verbose_name='상세주소')
+    phone = models.CharField(max_length=20, blank=True, verbose_name='전화번호')
+    email = models.EmailField(null=True, blank=True, verbose_name='이메일')
     final_education = models.CharField(
         max_length=1,
         choices=FINAL_EDUCATION_CHOICES,
         default='e',
+        verbose_name='최종학력',
     )
-    former_occupation = models.CharField(max_length=128, blank=True)
-    desired_occupation1 = models.CharField(max_length=128)
-    desired_occupation2 = models.CharField(max_length=128, blank=True)
-    desired_occupation3 = models.CharField(max_length=128, blank=True)
-    desired_Salary = models.PositiveIntegerField(null=True, blank=True)
-    etc = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    former_occupation = models.CharField(max_length=128, blank=True, verbose_name='이전직업')
+    desired_occupation1 = models.CharField(max_length=128, verbose_name='1.희망직업(필수)')
+    desired_occupation2 = models.CharField(max_length=128, blank=True, verbose_name='2.희망직업')
+    desired_occupation3 = models.CharField(max_length=128, blank=True, verbose_name='3.희망직업')
+    desired_Salary = models.PositiveIntegerField(null=True, blank=True, verbose_name='희망월급')
+    etc = models.TextField(blank=True, verbose_name='기타사항')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='생성일')
+    updated = models.DateTimeField(auto_now=True, verbose_name='수정일')
 
